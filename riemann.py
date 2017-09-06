ac484c051491a9e2db5c9f26fda974f469a8e829
Austin McDowell <austintmcdowell@berkeley.edu>
2017-09-06 13:04:08 -0400

    worked on riemann sum

--- riemann.py
+++ riemann.py
@@ -1,5 +1,8 @@
 # This is a python file! The '#' character indicates that the following line is a comment.
 
+import numpy as np
+
+
 # The following is an example for how to define a function in Python
 # def tells the compiler that hello_world is the name of a function
 # this implementation of hello_world takes a string as an argument,
@@ -13,3 +16,28 @@ def hello_world(name=''):
     
     
 #Implement the Riemann Sum approximation for integrals.
+
+def fn( x ):
+
+    y = x**2.
+    return y
+
+
+def r_mann( a , b , N, fn ):
+    """
+    a : starting point
+    b : ending point
+    N : number of rectangles
+    fn : input function
+    """
+    
+    width = (b - a) / N
+    print width
+    x = np.arange(a+width, b+width, width)
+    return x
+    
+    
+
+    
+    
+    
